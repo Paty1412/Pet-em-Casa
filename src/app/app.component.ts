e303ba92fdfbb6cdf886719853054db895062942
Paty1412 <patyevivi41@gmail.com>
2025-02-18 17:48:52 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { SobreComponent } from "./sobre/sobre.component";
+import { FooterComponent } from "./footer/footer.component";
+import {VisitaComponent} from './visita/visita.Component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent,SobreComponent,FooterComponent,RouterOutlet,VisitaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'creche';
+
 }
---
+++ src/app/sobre/sobre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: true,   // Torna o componente standalone
+  templateUrl: './sobre.component.html',
+  styleUrls: ['./sobre.component.css']
+})
+export class SobreComponent {
+ 
+}
